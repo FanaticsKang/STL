242b3f75aae0880d9291e54e33313ccd213ed55c
Yifei Kang <yifei.kang@zongmutech.com>
2019-07-13 15:16:59 +0800

    Set erase not exist element

--- Set/Test_Set.cpp
+++ Set/Test_Set.cpp
@@ -9,7 +9,12 @@ int main() {
   for (int i = 0; i < 10; ++i) {
     hello_set.emplace(i);
   }
-  hello_set.emplace(1);
+  hello_set.emplace(1);  // not insert because it has
+  for (auto &iter : hello_set) {
+    std::cout << iter << std::endl;
+  }
+  hello_set.erase(5);
+  hello_set.erase(50);// erase not exist
   for (auto &iter : hello_set) {
     std::cout << iter << std::endl;
   }
